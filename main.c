8eeb1094c25c43a40a00f0d9ceaf8671c7884ab2
olibouv <oli.bouv@yahoo.fr>
2017-05-22 23:40:30 +0200

    Update main.c

--- main.c
+++ main.c
@@ -9,11 +9,19 @@
 
 int main()
 {
-	char mot[] = "bonjour";
+	/* arbre_t * b = NULL;*/
 	arbre_t * a = (arbre_t *) malloc(sizeof(arbre_t));
 	a->lettre = 'a';
 	a->lh = NULL;
 	a->lv = NULL;
-	insert(a,mot,7);
+	insert(&a,"bonjouR",7);
+	insert(&a,"boN",3);
+	insert(&a,"bonsoiR",7);
+	insert(&a,"arbrE",5);
+	insert(&a,"arborescenT",11);
+	insert(&a,"arboricolE",10);
+	libere(a);
+	/insert(&b,"saluT",5);
+	libere(b);/
 	return 0;
 }
