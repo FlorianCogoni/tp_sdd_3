80efe1f9146006e8c9909ea6074fbfae7a56d861
olibouv <oli.bouv@yahoo.fr>
2017-05-22 23:55:17 +0200

    Update insertion.h

--- insertion.h
+++ insertion.h
@@ -2,7 +2,8 @@
 #define INSERTION
 
 arbre_t ** recherche(arbre_t * a, char mot[], int l,int * i);
-void insert(arbre_t * a, char mot[], int l );
+void insert(arbre_t ** a, char mot[], int l );
 void insertionFichierTexte(arbre_t * a, char nomFichier[]);
+void liberer(arbre_t * a);
 
 #endif
