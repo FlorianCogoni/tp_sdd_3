164792f6d6fb3b3f1447d4d1c27c0a747e3cd89a
olibouv <oli.bouv@yahoo.fr>
2017-05-29 00:32:17 +0200

    Add files via upload

--- recherche.c
+++ recherche.c
@@ -1,3 +1,17 @@
+/*
+ ______________________________________________________________________________________
+|                                                                                      |
+|                              ___________________                                     |
+|                             |                   |                                    |
+|                             |    recherche.c    |                                    |
+|                             |___________________|                                    |
+|                                                                                      |
+|______________________________________________________________________________________|
+
+	Ce fichier contient les fonctions pour la question 3 : la recherche.
+
+*/
+
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
@@ -6,30 +20,38 @@
 #include "insertion.h"
 #include "affichage.h"
 
+#define NB_BLOCS 200 /* nombre de blocs (de l'arbre) maximal */
+
+/*---------------------------------------------------------------------
+
+ Cette fonction est une variante d'affichage, elle affiche le motif puis chaque
+ mot du sous arbre.
+
+*/
 
 void affichageAvecMotif(arbre_t * a, char motif[])
 {
-	int fin=0;
+	int fin=0; /* bouléen qui marque la fin */
 	arbre_t * cour = a;
-	pile_t * pPile = initPile(50);
+	pile_t * pPile = initPile(NB_BLOCS); /* la pile va nous servir à revenir aux pères */
 	while (!fin)
 	{
-		while (cour != NULL)
+		while (cour != NULL) /* tant qu'on est pas au bout d'une branche */
 		{
 			empile(pPile,cour); /* on empile les adresses */
-      			if (isupper(cour->lettre))  /*on affiche seulement les mots qui finissent par une majuscule*/
+      			if (isupper(cour->lettre))  /* on affiche seulement les mots qui finissent par une majuscule */
 			{
-        printf("%s",motif);
-				afficheMot(pPile);
+        printf("%s",motif); /* on affiche d'abord le motif */
+				afficheMot(pPile); /* avant d'afficher le mot */
 			}
 			cour = cour->lv; /* on passe au fils (lien vertical) */
 		}
-		if (! estVide(pPile))
+		if (! estVide(pPile)) /* si il reste des frères */
 		{
 			depile(pPile,&cour);
 			cour=cour->lh; /* on passe au frère (lien horizontal) */
 		}
-		else
+		else /* il ne reste pas de frère, le parcours est fini */
 		{
 			fin = 1;
 		}
@@ -38,17 +60,23 @@ void affichageAvecMotif(arbre_t * a, char motif[])
 }
 
 
-void rechercheMotif(arbre_t * a, char motif[],int l)
+/*---------------------------------------------------------------------
+
+ Cette fonction affiche les mots qui commençent par motif.
+ 	Elle prend en entrée la longueur du motif.
+
+*/
+
+void rechercheMotif(arbre_t * a, char motif[], int l)
 {
-  /*int l = strlen(motif);*/
-  int i = 0;
+  int i = 0; /* ne sert pas mais doit être mis en paramètre de recherche */
   int parente = 0;
-  arbre_t * adr_motif = recherche(a, motif, l,&i,&parente);
-	if (parente == 1)
+  arbre_t * adr_motif = recherche(a, motif, l, &i, &parente); /* on récupère l'adresse du précédent de motif */
+	if (parente == 1) /* le motif est en lien vertical */
 	{
 		affichageAvecMotif(adr_motif->lv,motif);
 	}
-	else
+	else /* le motif est en lien horizontal */
 	{
 		affichageAvecMotif(adr_motif->lh,motif);
 	}
--- struct.h
+++ struct.h
@@ -1,11 +1,26 @@
+/*
+ ______________________________________________________________________________________
+|                                                                                      |
+|                              ___________________                                     |
+|                             |                   |                                    |
+|                             |      struct.h     |                                    |
+|                             |___________________|                                    |
+|                                                                                      |
+|______________________________________________________________________________________|
+
+	Ce fichier contient la structure de l'arbre.
+
+*/
+
+
 #ifndef STRUCTURE
 #define STRUCTURE
 
 typedef struct arbre
 {
-	char 		         lettre;
-	struct arbre   * lv;
-	struct arbre   * lh;
+	char 		         lettre;	/* la lettre */
+	struct arbre   * lv;			/* lien vertical */
+	struct arbre   * lh;			/* lien horizontal */
 } arbre_t;
 
 #endif
