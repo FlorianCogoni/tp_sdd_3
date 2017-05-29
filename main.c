ba8e10fb269bb96c64f50d95cb9217461714f2e7
FlorianCogoni <floco10@hotmail.fr>
2017-05-29 02:12:01 +0200

    hold up

--- main.c
+++ main.c
@@ -14,26 +14,40 @@ int main()
 	arbre_t * doublons = NULL;
 	arbre_t * vide = NULL;
 	arbre_t * mots_decroissants = NULL;
+
 	printf("test insertion depuis le fichier standard.txt\n\n");
 	insertionFichierTexte(&standard,"standard.txt");
 	printf("affichage de standard : \n\n");
 	affichage(standard);
 	printf("\n");
+
 	printf("test insertion depuis le fichier doublons.txt\n\n");
 	insertionFichierTexte(&doublons,"doublons.txt");
 	printf("affichage de doublons : \n\n");
 	affichage(doublons);
 	printf("\n");
+
 	printf("test insertion depuis le fichier vide.txt\n\n");
 	insertionFichierTexte(&vide,"vide.txt");
 	printf("affichage de vide : \n\n");
 	affichage(vide);
 	printf("\n");
+
 	printf("test insertion depuis le fichier mots_decroissants.txt\n\n");
 	insertionFichierTexte(&mots_decroissants,"mots_decroissants.txt");
-	printf("affichage de mots_croissants : \n\n");
+	printf("affichage de mots_decroissants : \n\n");
 	affichage(mots_decroissants);
 	printf("\n");
+
+	printf("test rechercheMotif sur standard avec le motif bo : \n");
+	rechercheMotif(standard,"bo",2);
+
+	printf("test rechercheMotif sur standard avec le motif abr : \n");
+	rechercheMotif(standard,"abr",3);
+
+	printf("test rechercheMotif sur standard avec le motif vide : \n");
+	rechercheMotif(standard,"",0);
+
 	printf("liberation des arbres \n");
 	liberer(standard);
 	liberer(doublons);
